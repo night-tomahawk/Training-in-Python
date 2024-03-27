93caa2d6287c41e2131eb2360e5b6e8dcbb3c1b8
night-tomahawk <Vyacheslav.t25@gmail.com>
2024-03-27 21:38:52 +0300

    addition training

--- src/Training.py
+++ src/Training.py
@@ -1,2 +1,7 @@
 n = int(input())
-print(n // 3)
+print(int(n // 3))
+# В гостях у бабушки
+n = int(input('Укажите количество квартир на этаже '))
+k = int(input('Введите номер квартиры '))
+floor = k // n + bool(k % n)
+print(floor)
