54ea39b73c19f81da0ed1cace4901f13acfa799a
night-tomahawk <Vyacheslav.t25@gmail.com>
2024-04-03 21:37:20 +0300

    create_0

---
+++ src/lesson fhree.py
@@ -0,0 +1,58 @@
+# Цикл while
+# n = int(input())
+# i = 1
+# while i <= n:
+#     print(i)
+#     i += 1
+# now = int(input())
+# now_min = now
+# while now != 0:
+#     if now < now_min:
+#         now_min = now
+#     now = int(input())
+# print(now_min)
+# count = 0
+# num = int(input())
+# while num != 0:
+#     if num % 10 == 1 and num % 4 == 0:
+#         count += 1
+#         num = int(input())
+#         print(count)
+# функция while используется когда цикл может повторяться неограниченное количество раз,
+# 'FOR' когда сам задаешь количество повторений
+# num = int(input())
+# while num > 0:
+#     print(num % 10)
+#     num //= 10
+# n = int(input())
+# for i in range(n):
+#     print(i)
+# # Определить сумму целых чисел от 0 до 101 с шагом 5
+# n = 0
+# for i in range(100, 0, -1):
+#     n += i
+# print(n)
+# i = 1
+# while True:
+#     print(i, end='')
+#     i = i + 1
+#     if i > 100:
+#         continue
+# n = int(input())
+# for i in range(3, n, 2):
+#     count = 0
+#     for j in range (2, int(i ** 0.5) + 1):
+#         if i % j == 0:
+#             count += 1
+#             break
+#     if count == 0:
+#         print(i)
+# x = -9
+# print(abs(x))
+# print(round(2.6))
+# word = input()
+# lenght = len(word)
+# print(lenght)
+# a = 10
+# print(dir(a))
+import this
